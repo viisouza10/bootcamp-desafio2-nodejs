85d8703af9eabc4c977d801b58eb122fea95d4cd
Vinicius Souza <viisouza10@live.com>
2020-07-02 10:37:04 -0300

    Post repositories

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+class repository {
+  constructor(title, url, techs) {
+    this.id = uuid();
+    this.title = title;
+    this.url = url;
+    this.techs = techs;
+    this.likes = 0;
+  }
+}
 
 const repositories = [];
 
@@ -15,7 +25,22 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  if (!title) {
+    return response.status(422).json({ error: `campo ${title} não enviado` });
+  }
+  if (!url) {
+    return response.status(422).json({ error: `campo ${url} não enviado` });
+  }
+  if (!techs) {
+    return response.status(422).json({ error: `campo ${techs} não enviado` });
+  }
+
+  const newRepositorie = new repository(title, url, techs);
+  repositories.push(newRepositorie);
+
+  return response.json(newRepositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
